4751ce0d1c58985be39fc6adad2c8209c82806f4
igor.zubarev <igor.zubarev@aspose.com>
2023-08-26 14:40:58 +0300

    update translation utils

---
+++ templates/utils/translate_add_java_landings .bat
@@ -0,0 +1,16 @@
+::call translate_json add_java de
+::call translate_json add_java es
+::call translate_json add_java fr
+::call translate_json add_java id
+::call translate_json add_java ja
+::call translate_json add_java ko
+::call translate_json add_java ru
+::call translate_json add_java zh-cn
+
+::call translate_json add_java bg
+::call translate_json add_java it
+::call translate_json add_java pl
+::call translate_json add_java pt
+::call translate_json add_java th
+::call translate_json add_java tr
+::call translate_json add_java vi
---
+++ templates/utils/translate_add_net_landings.bat
@@ -0,0 +1,16 @@
+::call translate_json add_net de
+::call translate_json add_net es
+::call translate_json add_net fr
+::call translate_json add_net id
+::call translate_json add_net ja
+::call translate_json add_net ko
+::call translate_json add_net ru
+::call translate_json add_net zh-cn
+
+::call translate_json add_net bg
+::call translate_json add_net it
+::call translate_json add_net pl
+::call translate_json add_net pt
+::call translate_json add_net th
+::call translate_json add_net tr
+::call translate_json add_net vi
